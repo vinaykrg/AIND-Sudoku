3742edf6b1edcc5f4ede9cb65e9121ef79827363
vinaykrg <34573278+vinaykrg@users.noreply.github.com>
2017-12-19 13:56:03 +0800

    t

--- solution.py
+++ solution.py
@@ -5,14 +5,7 @@ from utils import *
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-#diagonal_units_1 = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9']]
-#diagonal_units_2 = [['I1', 'H2', 'G3', 'F4', 'E5', 'D6', 'C7', 'B8', 'A9']]
-#unitlist = row_units + column_units + square_units + diagonal_units_1 + diagonal_units_2
-
-diag1 = [[str(r)+str(10- int(c)) for (r,c) in zip(rows,cols)]]
-diag2 = [[r+c for (r,c) in zip(rows,cols)]]
-unitlist = row_units + column_units + square_units + diag1 + diag2
-
+unitlist = row_units + column_units + square_units
 
 # TODO: Update the unit list to add the new diagonal units
 unitlist = unitlist
@@ -49,39 +42,7 @@ def naked_twins(values):
     strategy repeatedly).
     """
     # TODO: Implement this function!
-        # we first find all possible naked twins values on the board
-    all_digits = '123456789'
-    
-    for unit in row_units + column_units + square_units:
-        d = dict()
-        for digit in all_digits:
-            # get all boxes in the unit that have the digit
-            boxes_with_digit = ''.join([box for box in unit if digit in values[box]])
-            # if a digit is only in 2 boxes:
-            if len(boxes_with_digit) == 4:
-                # Insert the concatenated boxes names as the key in a dictionary with digit as the value 
-                if (boxes_with_digit not in d.keys()): 
-                    # print('inserting:', digit, ' in ', boxes_with_digit)
-                    d[boxes_with_digit] = digit
-                # If the concatenated boxes names are already in the dictionary, we have a twin!
-                else:
-                    digits = d[boxes_with_digit] + digit
-                    # print('updating:', digits, ' in ', boxes_with_digit)
-
-                    for box in unit:
-                        if (box in boxes_with_digit):
-                            # These 2 boxes must have only these 2 values
-                            assign_value(values, box, digits) #values[box] = digits
-                            
-                        else:
-                            # The rest of the boxes cannot have these digits
-                            assign_value(values, box, values[box].replace(digits[0], ""))
-                            assign_value(values, box, values[box].replace(digits[1], ""))
-                            # values[box] = values[box].replace(digits[0], "")
-                            # values[box] = values[box].replace(digits[1], "")
-    return values
-
-    #raise NotImplementedError
+    raise NotImplementedError
 
 
 def eliminate(values):
@@ -101,15 +62,7 @@ def eliminate(values):
         The values dictionary with the assigned values eliminated from peers
     """
     # TODO: Copy your code from the classroom to complete this function
-    solved_values = [box for box in values.keys() if len(values[box]) == 1]
-    for box in solved_values:
-        digit = values[box]
-        for peer in peers[box]:
-            assign_value(values, peer, values[peer].replace(digit,'')) 
-            #values[peer] = (values[peer].replace(digit,''))
-            return values
-    #pass
-    #raise NotImplementedError
+    raise NotImplementedError
 
 
 def only_choice(values):
@@ -133,18 +86,7 @@ def only_choice(values):
     You should be able to complete this function by copying your code from the classroom
     """
     # TODO: Copy your code from the classroom to complete this function
-    all_digits = '123456789'
-
-    for unit in unitlist:
-        for digit in all_digits:
-            # get all boxes in the unit that have the digit
-            boxes_with_digit = [box for box in unit if digit in values[box]]
-            # if there is only 1 box, update it
-            if len(boxes_with_digit) == 1:
-                assign_value(values, boxes_with_digit[0], digit) 
-                # values[boxes_with_digit[0]] = digit
-    return values
-    #raise NotImplementedError
+    raise NotImplementedError
 
 
 def reduce_puzzle(values):
@@ -162,26 +104,7 @@ def reduce_puzzle(values):
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
     # TODO: Copy your code from the classroom and modify it to complete this function
-    solved_values = [box for box in values.keys() if len(values[box]) == 1]
-    stalled = False
-    while not stalled:
-        # Check how many boxes have a determined value
-        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
-        # Use the Eliminate strategy
-        values = eliminate(values)
-        # Use the Only Choice Strategy
-        values = only_choice(values)
-        # Use the naked twins strategy
-        values = naked_twins(values) 
-        # Check how many boxes have a determined value, to compare
-        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
-        # If no new values were added, stop the loop.
-        stalled = solved_values_before == solved_values_after
-        # Sanity check, return False if there is a box with zero available values:
-        if len([box for box in values.keys() if len(values[box]) == 0]):
-            return False
-    return values
-    #raise NotImplementedError
+    raise NotImplementedError
 
 
 def search(values):
@@ -204,22 +127,7 @@ def search(values):
     and extending it to call the naked twins strategy.
     """
     # TODO: Copy your code from the classroom to complete this function
-    values = reduce_puzzle(values)
-    if values is False:
-        return False
-    # Chose one of the unfilled square s with the fewest possibilities
-    not_solved = [box for box in values.keys() if len(values[box]) > 1]
-    if len(not_solved) > 0:
-        box, vals = sorted(values.items(), key = lambda x: 10 if (len(x[1]) <= 1) else len(x[1]))[0]
-        for v in vals:
-            attempted_values = values.copy()
-            attempted_values[box] = v
-            attempted_sol = search(attempted_values)
-            if attempted_sol:
-                return attempted_sol
-    else:
-        return values
-    #raise NotImplementedError
+    raise NotImplementedError
 
 
 def solve(grid):
